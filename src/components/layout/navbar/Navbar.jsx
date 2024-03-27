a091fb93a71e15f1712a0e81d9798076e968bd52
juan mosqueda <j.ignaciomosqueda@gmail.com>
2024-03-27 17:12:56 -0300

    segundo comit, estilos agregados

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import { CartWidget } from "../../cartWidget/CartWidget";
+import "../../../index.css";
 
 export const Navbar = () => {
-  const Categorias = ["Juegos", "Categorias", "Ofertas", "epico"];
+  const Categorias = ["Juegos", "Categorias", "Ofertas"];
   return (
     <header>
       <nav>
         <h1>logo</h1>
         <ul>
           {Categorias.map((Categoria, index) => (
-            <li key={index}>
+            <li key={index} className="Pages">
               <a href={`#${Categoria}`}>{Categoria}</a>
             </li>
           ))}
